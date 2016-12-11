7dd38983ecea8333a09b0a23a45d7a8109ebf0f9
jklups <jeremy.klups@gmail.com>
2016-12-11 15:34:41 +0100

    Add route to show soft deleted quotes Add route to show steps of the kway travel Add route to add a step in the kway travel Add route to get the total distance of the travel

--- models/Kway.js
+++ models/Kway.js
@@ -1,9 +1,14 @@
 var mongoose = require('mongoose');
 
 var KwaySchema = new mongoose.Schema({
-  user: String,
-  location: String,
-  distance: Number
+	user : String,
+	location : String,
+	distance : Number,
+	from : Date,
+	to : Date,
+	photos : [ {
+		photo : String
+	} ]
 });
 
 mongoose.model('Kway', KwaySchema);
--- routes/index.js
+++ routes/index.js
@@ -155,4 +155,56 @@ router.get('/quotes/byauthoruser/:authoruser/' + PAGE_PARAM + '/:' + PAGE_PARAM 
 	});
 });
 
+/*
+ * Show soft deleted quotes
+ */
+router.get('/quotes/deleted', function(req, res, next) {
+	Quote.find({deletionDate: {$exists: true}}).sort('-deletionDate').exec(function(err, quotes) {
+		if (err) {
+			console.log("Unable to get deleted quotes because of " + err);
+			return next(err);
+		}
+		res.json(quotes);
+	});
+});
+
+/*
+ * Show all Kway
+ */
+router.get('/kways', function(req, res, next) {
+	Kway.find().sort('-to').exec(function(err, quotes) {
+		if (err) {
+			console.log("Unable to get list of kway because of " + err);
+			return next(err);
+		}
+		res.json(quotes);
+	});
+});
+
+/*
+ * Calculate the distance that kway travels
+ */
+router.get('/kways/distance', function(req, res, next) {
+	Kway.aggregate( { $group: { _id: null, totalDistance: { $sum: "$distance"} }} ).exec(function(err, totalDistance) {
+		if (err) {
+			console.log("Unable to get distance that kway traveled because of " + err);
+			return next(err);
+		}
+		res.json(totalDistance);
+	});
+});
+
+/* Add a new kway */
+router.put('/kway', function (req, res, next) {
+	var kway = new Kway(req.body);
+	
+	kway.save(function(err, pos) {
+		if (err) {
+			console.log("Unable to add kway " + kway + " because of " + err);
+			return next(err);
+		}
+		res.json(kway);
+	});
+});
+
 module.exports = router;
